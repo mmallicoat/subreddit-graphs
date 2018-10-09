c6eb7d328f8ef7534371486a245b0226b6c43fb4
MM <mm@marshallmallicoat.com>
2018-10-09 11:54:34 -0500

    Updated 'div' classes for fields; spider works again

--- subscraper/spiders/subspider.py
+++ subscraper/spiders/subspider.py
@@ -1,31 +1,39 @@
 import scrapy
 import re
+import pdb
 
 
 class QuotesSpider(scrapy.Spider):
     name = 'subspider'
     start_urls = [
-        'https://www.reddit.com/r/financialindependence',
+        'https://www.reddit.com/r/programming',
+        # 'https://www.reddit.com/r/financialindependence',
     ]
 
     def parse(self, response):
-        for sidebar in response.css('div.venmmj-0'):
+
+        # CSS classes of div's in sidebar change frequently!
+        for sidebar in response.css('div.s1s8pi67-0'):
+            sub_count = subscriber_conversion(
+                            sidebar.css('p.s34nhbn-12::text').extract_first()
+                        )
+            links = list()
+            for link in response.css('div.s1s8pi67-0 a::attr(href)').re('/r/\w+'):
+                links.append(link.lower())
             yield {
-              'subreddit': re.search(r'/r/\w+', response.request.url).group(),
-              'description': sidebar.css('p.s1smssg-14::text').extract_first(),
-              # TODO: change link strings to lowercase to match subreddit names
-              'links': response.css('div.venmmj-0 a::attr(href)').re('/r/\w+'),
-              'subscribers': subscriber_conversion(
-                             sidebar.css('p.s1smssg-12::text').extract_first(),
-                             )
+              'subreddit': re.search(r'/r/\w+', response.request.url).group().lower(),
+              'description': sidebar.css('p.s34nhbn-14::text').extract_first(),
+              'links': links,
+              'subscribers': sub_count
             }
 
         # List is of relative urls to subreddits, e.g., u'/r/foobar/'
-        for sub in response.css('div.venmmj-0 a::attr(href)').re('/r/\w+'):
-            yield response.follow(sub, callback=self.parse)
+        for sub in response.css('div.s1s8pi67-0 a::attr(href)').re('/r/\w+'):
+            yield response.follow(sub.lower(), callback=self.parse)
 
 def subscriber_conversion(subscriber_string):
-    number = float(re.search(r'[0-9\.]+', subscriber_string))
+    # pdb.set_trace()
+    number = float(re.search(r'[0-9\.]+', subscriber_string).group())
     if subscriber_string[-1] == 'k':
         subscriber_numeric = number * 1000
     elif subscriber_string[-1] == 'm':    
