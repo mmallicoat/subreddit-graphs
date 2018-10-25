775f2cb9c0a9aa359658f4d022ef262a9c8b6450
MM <mm@marshallmallicoat.com>
2018-10-25 09:42:07 -0500

    Got scrapy working again, although must be called from scrapy project root.

--- src/data/scraper/settings.py
+++ src/data/scraper/settings.py
@@ -14,7 +14,6 @@ BOT_NAME = 'scraper'
 SPIDER_MODULES = ['scraper.spiders']
 NEWSPIDER_MODULE = 'scraper.spiders'
 
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 # USER_AGENT = ''
 
